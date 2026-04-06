6fe939a89e7a37974cd134ca7f03f797534bd85e
Nikita Ignatyev <72838776+ignatyevnik@users.noreply.github.com>
2026-04-07 00:46:15 +0300

    Delete setup-auth.sh

--- setup-auth.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-source ../.env
-htpasswd_file="../nginx/.htpasswd"
-
-echo "🔐 Генерация .htpasswd..."
-htpasswd -bc "$htpasswd_file" "$NGINX_ADMIN_USER" "$NGINX_ADMIN_PASSWORD"
-chmod 644 "$htpasswd_file"
-echo "✅ Файл создан: $htpasswd_file"
